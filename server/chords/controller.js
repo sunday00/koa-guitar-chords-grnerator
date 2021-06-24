aff3520b89d857527a8355f44ff54347ec267e17
sunday00 <kind65505256@gmail.com>
2021-06-24 13:42:28 +0900

    model

--- server/chords/controller.js
+++ server/chords/controller.js
@@ -1,29 +1,32 @@
-const fs = require('fs');
+const fs = require("fs");
 const _ = require("underscore");
 
-exports.list = async (ctx, next) => {
-    let data = fs.readFileSync(__dirname+'/../tempDB.json', 'utf8');
-   
-    ctx.body = _.where(
-            JSON.parse(data),
-            {setName:ctx.request.query.provider}
-        )[0]
-        .sets
-    ;
+const db = require("../database");
 
-    await next();
+exports.list = async (ctx, next) => {
+  // let data = fs.readFileSync(__dirname + "/../tempDB.json", "utf8");
+
+  // ctx.body = _.where(JSON.parse(data), {
+  //   setName: ctx.request.query.provider,
+  // })[0].sets;
+
+  let data = await db.chords.findAll({
+    include: {
+      model: db.providers,
+    },
+  });
+
+  console.log(data);
+
+  await next();
 };
 
 exports.read = async (ctx, next) => {
-    let data = fs.readFileSync(__dirname+'/../tempDB.json', 'utf8');
-   
-    ctx.body = _.where(
-            JSON.parse(data),
-            {setName:ctx.request.query.provider}
-        )[0]
-        .sets
-        .filter(s => s.name === ctx.request.query.id
-    )[0];
+  let data = fs.readFileSync(__dirname + "/../tempDB.json", "utf8");
 
-    await next();
+  ctx.body = _.where(JSON.parse(data), {
+    setName: ctx.request.query.provider,
+  })[0].sets.filter((s) => s.name === ctx.request.query.id)[0];
+
+  await next();
 };
