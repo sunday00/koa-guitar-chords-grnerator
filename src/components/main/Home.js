21078a605977f5a576cf707d6c314d65dbafb05d
sunday00 <kind65505256@gmail.com>
2021-06-24 18:09:46 +0900

    db

--- src/components/main/Home.js
+++ src/components/main/Home.js
@@ -1,14 +1,11 @@
 import React from "react";
 
 const Home = () => {
-
-    return ( 
-        <ul>
-            <a href="http://localhost:3000/chord/list/ytb-leaning-guitar">
-                list ytb-leaning-guitar
-            </a>
-        </ul>
-    );
+  return (
+    <ul>
+      <a href="http://localhost:3000/chord/list/1">list ytb-leaning-guitar</a>
+    </ul>
+  );
 };
 
 export default Home;
