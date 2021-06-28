7128ca47545a5b711f48ef8d4ed32a663a46c4ed
sunday00 <kind65505256@gmail.com>
2021-06-28 17:05:31 +0900

    riff form

--- src/containers/riffs/RiffCreateContainer.js
+++ src/containers/riffs/RiffCreateContainer.js
@@ -66,7 +66,7 @@ const RiffCreateContainer = ({ match }) => {
       },
     ],
     riffOption: {
-      text: "1~2line X 2 ? then go to 3",
+      text: "",
     },
     memo: "",
   });
@@ -79,38 +79,45 @@ const RiffCreateContainer = ({ match }) => {
     return chords.find((c) => c.name === name);
   };
 
-  const handleChangeTab = (e, tab, field) => {
+  const handleChangeTab = (e, tab, field, options = null) => {
     const newTabs = Array.from(riff.tabs).map((t, i) => {
-      if (i === tab) {
+      if (i + 1 === tab) {
+        let v;
+        switch (field) {
+          default:
+            v = e.target.value;
+            break;
+          case "beat":
+            v = parseInt(e.target.value);
+            break;
+          case "options":
+            v = options;
+            break;
+        }
+
         return {
           ...t,
-          [field]: e.target.value,
+          [field]: v,
         };
       }
       return t;
     });
+
     setRiff({
       ...riff,
       tabs: newTabs,
     });
   };
 
-  const handleChangeOptions = (e, field) => {
+  const handleChangeOptions = (e, field, v) => {
     setRiff({
       ...riff,
-      [field]: e.target.value,
+      [field]: v,
     });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    // if (!riff.name) {
-    //   setErr({
-    //     ...err,
-    //     msg: "name is required",
-    //   });
-    // }
 
     setErr({
       ...err,
@@ -148,7 +155,158 @@ const RiffCreateContainer = ({ match }) => {
         );
       })}
 
-      <p className="riff-option-text">{riff.riffOption.text}</p>
+      {riff.tabs.map((t, i) => {
+        const ii = i + 1;
+        return (
+          <fieldset key={i}>
+            <label htmlFor={`chord${ii}`}>
+              <span>chord</span>
+              <select
+                name="chord"
+                id={`chord${ii}`}
+                className="chord"
+                onChange={(e) => handleChangeTab(e, ii, "chordName")}
+                value={riff.tabs[i].chordName}
+              >
+                {chords.map((c, y) => (
+                  <option key={y} value={c.name}>
+                    {c.name}
+                  </option>
+                ))}
+              </select>
+            </label>
+            <label htmlFor={`beat${ii}`}>
+              <span>beat</span>
+              <select
+                name="beat"
+                id={`beat${ii}`}
+                className={`beat${ii}`}
+                onChange={(e) => handleChangeTab(e, ii, "beat")}
+                value={riff.tabs[i].beat}
+              >
+                <option value={1}>1</option>
+                <option value={2}>2</option>
+                <option value={4}>4</option>
+                <option value={8}>8</option>
+                <option value={16}>16</option>
+              </select>
+            </label>
+            <label htmlFor={`turn${ii}`}>
+              <span>repeat</span>
+              <select
+                name="turn"
+                id={`turn${ii}`}
+                className={`turn${ii}`}
+                onChange={(e) =>
+                  handleChangeTab(e, ii, "options", {
+                    ...riff.tabs[i].options,
+                    turn: e.target.value,
+                  })
+                }
+                value={riff.tabs[i].options.turn}
+              >
+                <option value="">{""}</option>
+                <option value=">">{">"}</option>
+                <option value="<">{"<"}</option>
+                <option value="both">both</option>
+              </select>
+            </label>
+            <label htmlFor={`parm-mutes${ii}`}>
+              <span>p-mutes</span>
+              <input
+                type="text"
+                name={`parm-mutes${ii}`}
+                id={`parm-mutes${ii}`}
+                placeholder="divide by ,"
+                onChange={(e) =>
+                  handleChangeTab(e, ii, "options", {
+                    ...riff.tabs[i].options,
+                    parmMutes: [
+                      ...e.target.value.split(",").map((v) => parseInt(v)),
+                    ],
+                  })
+                }
+                defaultValue={riff.tabs[i].options.parmMutes.join(",")}
+              />
+            </label>
+            <label htmlFor={`up${ii}`}>
+              <span>up pick</span>
+              <input
+                type="text"
+                name={`up${ii}`}
+                id={`up${ii}`}
+                placeholder="divide by ,"
+                onChange={(e) =>
+                  handleChangeTab(e, ii, "options", {
+                    ...riff.tabs[i].options,
+                    upPicking: [
+                      ...e.target.value.split(",").map((v) => parseInt(v)),
+                    ],
+                  })
+                }
+                defaultValue={riff.tabs[i].options.upPicking.join(",")}
+              />
+            </label>
+            <label htmlFor={`rest${ii}`}>
+              <span>rest</span>
+              <input
+                type="text"
+                name={`rest${ii}`}
+                id={`rest${ii}`}
+                placeholder="divide by ,"
+                onChange={(e) =>
+                  handleChangeTab(e, ii, "options", {
+                    ...riff.tabs[i].options,
+                    rest: [
+                      ...e.target.value.split(",").map((v) => parseInt(v)),
+                    ],
+                  })
+                }
+                defaultValue={riff.tabs[i].options.rest.join(",")}
+              />
+            </label>
+            <label htmlFor={`vibe${ii}`}>
+              <span>vibe</span>
+              <input
+                type="text"
+                name={`vibe${ii}`}
+                id={`vibe${ii}`}
+                placeholder="divide by ,"
+                onChange={(e) =>
+                  handleChangeTab(e, ii, "options", {
+                    ...riff.tabs[i].options,
+                    vibe: [
+                      ...e.target.value.split(",").map((v) => parseInt(v)),
+                    ],
+                  })
+                }
+                defaultValue={riff.tabs[i].options.vibe.join(",")}
+              />
+            </label>
+          </fieldset>
+        );
+      })}
+
+      <input
+        className="riff-option-text"
+        onChange={(e) =>
+          handleChangeOptions(e, "riffOption", {
+            text: e.target.value,
+          })
+        }
+        value={riff.riffOption.text}
+        placeholder="some message"
+      />
+      <input
+        className="riff-option-memo"
+        onChange={(e) =>
+          handleChangeOptions(e, "memo", {
+            text: e.target.value,
+          })
+        }
+        value={riff.riffOption.memo}
+        placeholder="some memo"
+      />
 
       <input type="submit" className="submit" value="submit" />
 
